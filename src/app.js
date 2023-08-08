a68b7713cd1f850218bc310972de6c26f151047c
Ashwani Yadav <49113451+Ashwani-Ydv@users.noreply.github.com>
2023-08-08 15:40:18 +0530

    backend

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const router  = require("../src/Route/paymentRoute");
+const router = require("../src/Route/paymentRoute");
 const app = express();
 const path = require("path");
- 
+
 // app.set("view engine", "pug");
 // app.set("views", path.join(__dirname, "public"));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //port number
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use("/", router)
-app.use("/", (req, res) =>{
-    res.render('index')
-})
- 
-app.listen(PORT, () =>{
-    console.log(`Server is running successfully on PORT ${PORT}`);
-})
+app.use("/", router);
+app.use("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on PORT ${PORT}`);
+});
